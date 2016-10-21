1dcb7e6f7e9a24af1ec77206928ba78fc7a46541
Rasamalla, Sai Shashank <srasamal@contractor.jcp.com>
2016-10-21 17:03:51 -0500

    second commit

---
+++ src/app.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./header');
+var rootUrl= 'https://my-first-app-c71ec.firebaseio.com/';
+
+var App = React.createClass({
+    mixins: [ ReactFire ],
+    componentWillMount : function(){
+        
+        this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    },
+  render: function() {
+      
+    return <div className ="row panel panel-default">
+    <div className="col-md-8 col-md-offset-2">
+        <h2 className="text-center">
+        To-Do List
+        </h2>
+        <Header/>
+        
+        </div>
+    
+    </div> 
+  }
+});
+
+var element = React.createElement(App, {});
+ReactDOM.render(element, document.querySelector('.container'));
