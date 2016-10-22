cd1db4f1bd350ffddeff512d6c016f1ab4564a9c
shashank-r <saishashank.r@gmail.com>
2016-10-22 08:58:13 -0500

    adding form controls

--- src/app.jsx
+++ src/app.jsx
@@ -8,21 +8,21 @@ var rootUrl= 'https://my-first-app-c71ec.firebaseio.com/';
 var App = React.createClass({
     mixins: [ ReactFire ],
     componentWillMount : function(){
-        
+
         this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
     },
   render: function() {
-      
+
     return <div className ="row panel panel-default">
     <div className="col-md-8 col-md-offset-2">
         <h2 className="text-center">
         To-Do List
         </h2>
         <Header/>
-        
+
         </div>
-    
-    </div> 
+
+    </div>
   }
 });
 
--- src/header.jsx
+++ src/header.jsx
@@ -2,13 +2,31 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
+getInitialState:function(){
+  return{
+    text:''
+  }
+},
+
   render :function(){
       return <div className="input-group">
-      <input type="text" className="form-control"/>
+      <input
+        value={this.state.text}
+        onChange={this.handleInputChange}
+        type="text" className="form-control"/>
       <span className="input-group-btn">
-          <button className="btn btn-default" type="button">Add</button>
+          <button
+            onClick ={this.handleClick}
+            className="btn btn-default" type="button">Add</button>
           </span>
-      
+
       </div>
-  }  
+  },
+  handleClick:function() {
+    //Send Value
+
+  },
+  handleInputChange:function(event) {
+  this.setState({text:event.target.value});
+  }
 });
